5e55f8b039e2e4b2d6d21f02415b11ebe0924785
pablo <olbaplegal@gmail.com>
2026-04-22 11:32:16 -0300

    feat: add lombok annotations

--- src/main/java/com/cardapio/cardapio/food/Food.java
+++ src/main/java/com/cardapio/cardapio/food/Food.java
@@ -1,9 +1,19 @@
 package com.cardapio.cardapio.food;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "foods")
 @Entity(name = "foods")
+
+// in runtime lombok will generate the nexts boiler plaites
+@Getter // make a get for all the atributes
+@NoArgsConstructor // declare a constructor that receive not enven one the arguments
+@AllArgsConstructor // declare a constructor that receive all the arguments
+@EqualsAndHashCode(of = "id") // indicate that the id is the object's indentificator
 public class Food {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
