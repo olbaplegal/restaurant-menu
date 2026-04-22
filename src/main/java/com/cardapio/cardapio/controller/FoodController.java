247f7f4b11e7af0e4fb8a00f102e9a71f1574a9a
pablo <olbaplegal@gmail.com>
2026-04-22 11:50:45 -0300

    feat: add cors annotation

--- src/main/java/com/cardapio/cardapio/controller/FoodController.java
+++ src/main/java/com/cardapio/cardapio/controller/FoodController.java
@@ -16,6 +16,7 @@ public class FoodController {
     @Autowired
     private FoodRepository repository;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
     public void saveFood(@RequestBody FoodRequestDTO data){
         Food foodData = new Food(data);
@@ -23,7 +24,7 @@ public class FoodController {
         return;
     }
 
-
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public List<FoodResponseDTO> getAll(){
         List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
