b4d28a074bb234855449aa05fc10a52c33f3acc4
pablo <olbaplegal@gmail.com>
2026-04-22 11:00:55 -0300

    fix: import GenerationType

--- src/main/java/com/cardapio/cardapio/food/Food.java
+++ src/main/java/com/cardapio/cardapio/food/Food.java
@@ -1,9 +1,6 @@
 package com.cardapio.cardapio.food;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Table(name = "foods")
 @Entity(name = "foods")
