99d339ca68d211227b42833f1a8eb8dc80287948
pablo <olbaplegal@gmail.com>
2026-04-22 11:32:42 -0300

    feat: create getAll method

--- src/main/java/com/cardapio/cardapio/controller/FoodController.java
+++ src/main/java/com/cardapio/cardapio/controller/FoodController.java
@@ -1,4 +1,25 @@
 package com.cardapio.cardapio.controller;
 
+import com.cardapio.cardapio.food.Food;
+import com.cardapio.cardapio.food.FoodRepository;
+import com.cardapio.cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("com/cardapio/cardapio/food")
 public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+    }
 }
