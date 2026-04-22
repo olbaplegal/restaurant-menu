1f90c3df06a722049e3d300327f295feb24cda64
pablo <olbaplegal@gmail.com>
2026-04-22 11:45:46 -0300

    feat: create foodRequest method

--- src/main/java/com/cardapio/cardapio/controller/FoodController.java
+++ src/main/java/com/cardapio/cardapio/controller/FoodController.java
@@ -2,11 +2,10 @@ package com.cardapio.cardapio.controller;
 
 import com.cardapio.cardapio.food.Food;
 import com.cardapio.cardapio.food.FoodRepository;
+import com.cardapio.cardapio.food.FoodRequestDTO;
 import com.cardapio.cardapio.food.FoodResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +15,14 @@ public class FoodController {
 
     @Autowired
     private FoodRepository repository;
+
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+
 
     @GetMapping
     public List<FoodResponseDTO> getAll(){
--- src/main/java/com/cardapio/cardapio/food/Food.java
+++ src/main/java/com/cardapio/cardapio/food/Food.java
@@ -20,4 +20,10 @@ public class Food {
     private String title;
     private String image;
     private Integer price;
+
+    public Food(FoodRequestDTO data){
+        this.title = data.title();
+        this.image = data.image();
+        this.price = data.price();
+    }
 }
