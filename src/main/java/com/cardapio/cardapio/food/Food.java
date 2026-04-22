3edd1ef3644cb787323015142e28162d546349a2
pablo <olbaplegal@gmail.com>
2026-04-22 10:55:38 -0300

    feat: create db entity 'food'

---
+++ src/main/java/com/cardapio/cardapio/food/Food.java
@@ -0,0 +1,16 @@
+package com.cardapio.cardapio.food;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "foods")
+@Entity(name = "foods")
+public class Food {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String image;
+    private Integer price;
+}
