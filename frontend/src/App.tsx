01f9e153939bd24b17810b650dbd8677e837c48f
pablo <olbaplegal@gmail.com>
2026-04-24 18:07:55 -0300

    feat: add create modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import './App.css'
 import { Card } from './components/card/card';
 import { useFoodData } from './hooks/useFoodData';
+import { CreateModal } from './components/create-modal/create-modal';
 
 function App() {
   const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev)
+  }
 
   return(
     <div className="container">
@@ -18,6 +25,8 @@ function App() {
           />
         )}
       </div>
+        {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+        <button onClick={handleOpenModal}>Novo</button>
     </div>
   )
 }
